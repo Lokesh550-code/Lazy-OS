86ae8d92c0b5d02b8c5c44adbe4bea4dc673b647
Lokesh550-code <lok3sh2007@gmail.com>
2026-08-21 23:51:59 +0530

    feat: implement querry state for startup menu

--- src/taskbar/StartUpMenu.jsx
+++ src/taskbar/StartUpMenu.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import StartUpMenuApplication from "../components/StartUpMenuApplication";
 
-const StartUpMenu = ({ isFuzzelActive,startMenuRef, applicationRegistery }) => {
-  console.log(applicationRegistery)
+const StartUpMenu = ({
+  isFuzzelActive,
+  startMenuRef,
+  applicationRegistery,
+}) => {
+  const [querry, setQuerry] = useState("");
+
+  const handleInput = (e) => {
+    setQuerry(e.target.value);
+  };
+
   return (
     <div
       className={` ${isFuzzelActive ? `fixed` : `hidden`} font-application h-80 w-100 p-3 bottom-14 flex flex-col gap-3 bg-[#242222] text-[#f6e7d9] rounded`}
     >
-      <form className="w-full">
+      <form className="w-full" onSubmit={(e) => {e.preventDefault()}}>
         <input
+          onChange={handleInput}
+          value={querry}
           ref={startMenuRef}
           className="w-full pl-[35%] inherit focus:outline-none"
           type="text"
           name="querry"
-          id=""
           placeholder="Lazy Search"
         />
       </form>
-      {applicationRegistery.map((elem) => {
-        return <StartUpMenuApplication name={elem.name} key={elem.id} />
+      {applicationRegistery?.map((elem) => {
+        return <StartUpMenuApplication name={elem.name} key={elem.id} />;
       })}
     </div>
   );
