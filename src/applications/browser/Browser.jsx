c2f520844371f9f2dc44afa2b8798549253faf49
Lokesh550-code <lok3sh2007@gmail.com>
2026-08-26 12:55:06 +0530

    feat: add browser

--- src/applications/browser/Browser.jsx
+++ src/applications/browser/Browser.jsx
@@ -1,7 +1,37 @@
-const Browser = () => {
-  return (
-    <div className="h-full w-full bg-amber-200 text-black flex justify-center items-center">Browser</div>
-  )
-}
+import { useState } from "react";
+import SearchBar from "./SearchBar";
+import mascot from "../../assets/shinchan.png"
 
-export default Browser
+const Browser = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState("");
+  console.log(JSON.stringify(searchResult));
+  return (
+    <div className="h-full w-full bg-orange-300 text-black flex flex-col justify-center items-center">
+      <SearchBar
+        setSearch={setSearch}
+        search={search}
+        setSearchResult={setSearchResult}
+      />
+
+      {searchResult === "" ? (
+        <div className="h-full w-full text-xl text-[#242222] flex justify-center items-center select-none">
+          Lazy Browser: Crawl the internet
+          <br />
+          search to get started
+        </div>
+      ) : searchResult.trim().toLowerCase() === "laz is awesome" ? (
+        <div className="h-full w-full px-10 text-xl text-[#242222] flex flex-col justify-center items-center gap-3">
+          Ab mai itna bhi kuch Khas nahi, hehehe.
+          <img className="w-62.5" src={mascot} alt="Shinchan Blushing" />
+        </div>
+      ) : (
+        <div className="h-full w-full px-10 text-xl text-[#242222] flex justify-center items-center">
+          You really thought I would make a browser inside a browser?
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Browser;
---
+++ src/applications/browser/SearchBar.jsx
@@ -0,0 +1,47 @@
+import { RotateCw, ChevronLeft, ChevronRight } from "lucide-react";
+
+const SearchBar = ({ search, setSearch, setSearchResult }) => {
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  return (
+    <div className="h-11 w-full px-2 flex gap-2">
+      <div className="h-full flex justify-center items-center">
+        <button className="text-[#242222] hover:bg-orange-400 px-1 py-1 rounded transition cursor-pointer">
+          <ChevronLeft size={19} />
+        </button>
+        <button className="text-[#242222] hover:bg-orange-400 px-1 py-1 rounded transition cursor-pointer">
+          <ChevronRight size={19} />
+        </button>
+        <button
+          onClick={(e) => {
+            setSearchResult("");
+          }}
+          className="text-[#242222] hover:bg-orange-400 px-1 py-1 rounded transition cursor-pointer"
+        >
+          <RotateCw size={19} />
+        </button>
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSearchResult(search);
+          setSearch("");
+        }}
+        className="h-full w-full py-1"
+      >
+        <input
+          onChange={(e) => {
+            handleChange(e);
+          }}
+          value={search}
+          className="h-full w-full px-2 bg-[#242222] text-[#f6e7d9] font-application border-[#c05037] border-2 outline-none rounded"
+          type="text"
+          placeholder="Search..."
+        />
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
